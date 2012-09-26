08c8ccbeaef03c798a2c3eabe315544e5674a817
Patty <pattus1901@hotmail.com>
2012-09-26 20:28:46 -0300

    Los nuevos cambios realizados.. cree una carpeta para guardar las imagenes y solo esta hecha la pagina de inicio igual le falta completar mucho

---
+++ Pag_Inicio.php
@@ -0,0 +1,55 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<?php 
+ ini_set("display_errors","On");
+    error_reporting(E_ALL);
+ ?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Autos Provinciales</title>
+    </head>
+    <body background="Imagenes/escudo.png"> 
+        <h3>Manejo de Automotores Provincial</h3>
+        
+         <img src= "Imagenes/foto-de-auto-electrico.jpg"/> 
+               
+            	
+                <form title="Ingresar" action="LoginServlet" method="post">
+                	<table>
+                    	<tr>
+                        	<td>Usuario:</td><td><input type="text" value="" name="usuario"></td>
+                    	</tr>
+                        <tr>
+                        	<td>Clave:</td><td><input type="password" value="" name="clave"></td>
+                        </tr>
+                        <tr>
+                        	<td><input type="submit" value="Ingresar" button></td>
+                        
+                        </tr>  					
+                	</table>  				
+                </form> 
+        
+		
+
+   <!--   <div id="menu">
+        	<ul>
+            	<li id="menu1"><a href="index.html">Inicio</a></li>
+                <li id="menu2"><a href="empresa.html">La Empresa</a></li>
+                <li id="menu3"><a href="atencion_cliente.html">Atencion al Cliente</a></li>
+                <li id="menu4"><a href="intranet.html">Intranet</a></li>
+            </ul>
+        </div>
+   -->
+		
+		
+			<fieldset>
+				Todos los derechos reservados<br>
+                Nestor Aspiroz. |  Estrada Patricia
+                        </fieldset>
+    </body>
+</html>
